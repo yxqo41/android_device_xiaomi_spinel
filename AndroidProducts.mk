014b548a806446d59e8b10571b9ec294102643ee
yxseen.pptx <150557468+yxqo41@users.noreply.github.com>
2026-08-14 16:13:51 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_spinel.mk
 
@@ -13,4 +12,3 @@ COMMON_LUNCH_CHOICES := \
     fox_spinel-user \
     fox_spinel-userdebug \
     fox_spinel-eng
-
