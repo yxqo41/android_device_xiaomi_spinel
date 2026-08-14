268bab939575e63f529bee05e5800c42a0d42651
yxseen.pptx <150557468+yxqo41@users.noreply.github.com>
2026-08-14 18:25:22 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,11 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.common
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.common \
-    libgptutils \
-    libz \
-    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
