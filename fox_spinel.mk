2fb46b16d487551f64cf3496e39b72a5c9a712c9
yxseen.pptx <150557468+yxqo41@users.noreply.github.com>
2026-08-14 16:00:32 +0300

    Update fox_spinel.mk

--- fox_spinel.mk
+++ fox_spinel.mk
@@ -5,21 +5,33 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/xiaomi/spinel
-# A/B
+# -----------------------------------------------------------------
+# 1. INHERIT CONFIGURATIONS
+# -----------------------------------------------------------------
+# Inherit OrangeFox common rules
+$(call inherit-product, vendor/recovery/config/common.mk)
+
+# Inherit device setup
+$(call inherit-product, device/xiaomi/spinel/device.mk)
+
+# -----------------------------------------------------------------
+# 2. A/B & UPDATE ENGINE PACKAGES
+# -----------------------------------------------------------------
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.common
+    android.hardware.boot@1.0-service \
+    bootctrl.common \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.common \
@@ -27,9 +39,18 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libz \
     libcutils
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+# -----------------------------------------------------------------
+# 3. PRODUCT BRANDING & DENSITY
+# -----------------------------------------------------------------
+PRODUCT_NAME := fox_spinel
+PRODUCT_DEVICE := spinel
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 15 4G
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Security patch alignment
+PLATFORM_SECURITY_PATCH := 2026-06-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
